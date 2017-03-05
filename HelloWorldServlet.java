5c5dd615e6c8b87951ea9b1cfea1f037af9b21f6
Olga Pasynkova <olga.pasynkova@gmail.com>
2017-03-05 10:14:41 +0100

    HelloWorld  servlet

--- HelloWorldServlet.java
+++ HelloWorldServlet.java
@@ -13,11 +13,27 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=utf-8");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
 
-        PrintWriter pw = resp.getWriter();
-        pw.println("<H1>Hello, world! или Привет мир</H1>");
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                "transitional//en\">\n";
+
+        out.println(docType + "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>Words for cloud</b>: "
+                + request.getParameter("words_for_map") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+
+
 
     }
+
 }
